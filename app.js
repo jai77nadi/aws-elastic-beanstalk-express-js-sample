cff3110eef57220df7768d7bf153f83a546b11d3
jai77nadi <76108055+jai77nadi@users.noreply.github.com>
2020-12-16 21:41:08 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Java World!'));
+app.get('/', (req, res) => res.send('Hello Python World!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
